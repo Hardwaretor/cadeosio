508e1bc359619c29a98c565cabce0ea4a845d777
CADEOS <g.galindo@cadeos.io>
2019-08-15 02:26:07 +0200

    app.components

---
+++ var/www/cadeosio/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cadeosio';
+}
+
+import ScatterJS from '@scatterjs/core';
+import ScatterEOS from '@scatterjs/eosjs';
+
+ScatterJS.plugins( new ScatterEOS() );
+
+const network = ScatterJS.Network.fromJson({
+  blockchain:'eos',
+  chainId:'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+  host:'nodes.get-scatter.com',
+  port:443,
+  protocol:'https'
+});
+
+ScatterJS.connect('CADEOS.io', {network}).then(connected => {
+  if(!connected) return false;
+  // ScatterJS.someMethod();
+});
