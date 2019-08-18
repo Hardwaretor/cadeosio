890018899dfcfccff5fbaa4024678d528d0b9c38
CADEOS <g.galindo@cadeos.io>
2019-08-18 19:52:08 +0200

    Push all

--- var/www/cadeosio/src/app/AppRoutingModule.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { routes } from './app-routing.module';
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule {
-}
