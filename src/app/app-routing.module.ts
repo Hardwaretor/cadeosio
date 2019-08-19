79dd043543d9911152e1bec8ce88ab4bc3d9bd37
CADEOS <g.galindo@cadeos.io>
2019-08-19 03:27:53 +0200

    App design

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(Routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
