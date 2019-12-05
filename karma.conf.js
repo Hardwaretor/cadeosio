502580fef47ceeb0a2168e524a986529632bf7b1
CADEOS.io <40882121+CADEOS@users.noreply.github.com>
2019-12-05 12:52:38 -0600

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -11,6 +11,7 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
+      ]
       
   } 
 });
