376699764de998d39069e9aeeaf2e1d633d919de
CADEOS.io <40882121+CADEOS@users.noreply.github.com>
2019-12-05 14:13:38 -0600

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,15 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      restartOnFileChange: true,
+      restartOnFileChange: true,
+      customLaunchers: {
+      ChromeHeadlessCustom: {
+      base: 'ChromeHeadless',
+      flags: ['--no-sandbox', '--disable-gpu']
+  }
+},
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
