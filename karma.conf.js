d5329ffb96b063d8f77260fb319a670cd75ac34a
CADEOS.io <40882121+CADEOS@users.noreply.github.com>
2019-12-05 12:39:32 -0600

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,6 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
-      require('ts-node')
       compilerOptions: { 
       module: 'commonjs' 
       restartOnFileChange: true,
