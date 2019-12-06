031afea0124c43c90af82176c75a47edbeaef1a1
CADEOS <g.galindo@cadeos.io>
2019-12-05 20:29:35 -0600

    dapp

--- src/app/services/scatter.service.ts
+++ src/app/services/scatter.service.ts
@@ -8,7 +8,7 @@ import ScatterJS from 'scatterjs-core';
 import ScatterEOS from 'scatterjs-plugin-eosjs';
 import ScatterLynx from '@scatterjs/lynx';
 
-const defaultPrivateKey = '5JtUScZK2XEp3g9gh7F8bwtPTRAkASmNrrftmx4AxDKD5K4zDnr'; // bob
+const defaultPrivateKey = '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'; // bob
 const signatureProvider = new JsSignatureProvider([defaultPrivateKey]);
 const rpc = new JsonRpc('http://127.0.0.1:8888', { fetch });
 const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
