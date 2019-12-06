2dfb531de27621f7841bf28720ae1fafb0592c7a
CADEOS <g.galindo@cadeos.io>
2019-12-05 20:59:42 -0600

    app

--- src/environments/environment.hmr.ts
+++ src/environments/environment.hmr.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   hmr: true,
   eosHost: 'nodes.get-scatter.com',
-  apiUrl: '//api.cadeos.io',
+  apiUrl: '127.0.0.1:8888',
   eosPort: 443,
   eosProtocol: 'https'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   hmr: false,
   eosHost: 'nodes.get-scatter.com',
-  apiUrl: '//api.cadeos.io',
+  apiUrl: '127.0.0.1:8888',
   eosPort: 443,
   eosProtocol: 'https'
 };
